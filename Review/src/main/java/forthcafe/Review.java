c2fd7db830c4960586e2e45d9f2bce6954556a9d
antlgkrh <30856023+antlgkrh@users.noreply.github.com>
2021-03-03 20:58:26 +0900

    Add files via upload

---
+++ Review/src/main/java/forthcafe/Review.java
@@ -0,0 +1,36 @@
+package forthcafe;
+
+import javax.persistence.*;
+import org.springframework.beans.BeanUtils;
+import java.util.List;
+
+@Entity
+@Table(name="Review_table")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @PrePersist
+    public void onPrePersist(){
+        Reviewed reviewed = new Reviewed();
+        BeanUtils.copyProperties(this, reviewed);
+        reviewed.publishAfterCommit();
+
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+
+
+}
---
+++ Review/src/main/java/forthcafe/ReviewRepository.java
@@ -0,0 +1,8 @@
+package forthcafe;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ReviewRepository extends PagingAndSortingRepository<Review, Long>{
+
+
+}
