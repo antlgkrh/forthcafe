535a527c7805f7408584d534ab60d282d9ab0679
antlgkrh <30856023+antlgkrh@users.noreply.github.com>
2021-03-04 01:43:48 +0900

    Add files via upload

---
+++ Delivery/src/main/java/forthcafe/external/Review.java
@@ -0,0 +1,14 @@
+package forthcafe.external;
+
+public class Review {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ Delivery/src/main/java/forthcafe/external/ReviewService.java
@@ -0,0 +1,17 @@
+
+package forthcafe.external;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Date;
+
+@FeignClient(name="Review", url="http://Review:8080")
+public interface ReviewService {
+
+    @RequestMapping(method= RequestMethod.GET, path="/reviews")
+    public void review(@RequestBody Review review);
+
+}
